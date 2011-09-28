aefb4800dd6fd1179b610ad7f0f63d4313f8d199
dough <doug.harris@gmail.com>
2011-09-27 23:30:47 -0500

    Added a -p preface to the showcode.sh

--- showcode.sh
+++ showcode.sh
@@ -1,1 +1,1 @@
-javad net.dougharris.utility.ShowCode -o ShowCode.html -h ShowCode -t ShowCode -z ShowCode.zip -m ShowCode.mkd ShowCode.java ShowCode.mkd showcode.sh
+javad net.dougharris.utility.ShowCode -o ShowCode.html -h ShowCode -p preface -t ShowCode -z ShowCode.zip -m ShowCode.mkd ShowCode.java ShowCode.mkd showcode.sh
